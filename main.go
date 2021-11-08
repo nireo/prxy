761522e209dbfa6b55d5826b084f3d32d452ae2e
nireo <eemil.lehtikangas1@gmail.com>
2021-11-08 17:09:16 +0200

    added healthchecking

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	port := flag.Int("port", 8080, "the port where the load balancer should be hosted on")
+
+	log.Printf("starting the service on port: %d\n", *port)
 }
--- server/server.go
+++ server/server.go
@@ -1,18 +1,24 @@
 package server
 
 import (
+	"log"
+	"net"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync"
 	"sync/atomic"
+	"time"
 )
+
+var pool Pool
 
 // Server represents one of the configurable servers which trafic can be forwarded to.
 type Server struct {
 	sync.RWMutex
-	RP      *httputil.ReverseProxy
-	IsAlive bool
-	URL     *url.URL
+	RP    *httputil.ReverseProxy
+	Alive bool
+	URL   *url.URL
 }
 
 type Pool struct {
@@ -25,12 +31,25 @@ func (p *Pool) NextPos() uint64 {
 	return atomic.AddUint64(&p.curr, uint64(1)) % uint64(len(p.servers))
 }
 
+func (s *Server) SetAlive(v bool) {
+	s.Lock()
+	s.Alive = v
+	s.Unlock()
+}
+
+func (s *Server) IsAlive() bool {
+	s.RLock()
+	value := s.Alive
+	s.RUnlock()
+	return value
+}
+
 func (p *Pool) NextServer() *Server {
 	index := p.NextPos()
 	for i := index; i < (uint64(len(p.servers)) + index); i++ {
 		idx := i % uint64(len(p.servers))
 
-		if p.servers[idx].IsAlive {
+		if p.servers[idx].Alive {
 			if i != index {
 				atomic.StoreUint64(&p.curr, idx)
 			}
@@ -40,4 +59,48 @@ func (p *Pool) NextServer() *Server {
 	}
 
 	return nil
+}
+
+func Balance(w http.ResponseWriter, r *http.Request) {
+	serv := pool.NextServer()
+	if serv != nil {
+		serv.RP.ServeHTTP(w, r)
+		return
+	}
+
+	http.Error(w, "cannot access service", http.StatusServiceUnavailable)
+}
+
+func AliveCheckURL(u *url.URL) bool {
+	conn, err := net.DialTimeout("tcp", u.Host, 2*time.Second)
+	if err != nil {
+		log.Println("cannot reach server: ", err)
+		return false
+	}
+
+	conn.Close()
+	return true
+}
+
+func backgroundHealthCheck() {
+	t := time.NewTicker(time.Minute * 3)
+	for {
+		select {
+		case <-t.C:
+			log.Printf("checking server health...\n")
+
+			for _, b := range pool.servers {
+				status := "up"
+				alive := AliveCheckURL(b.URL)
+				b.SetAlive(alive)
+				if !alive {
+					status = "down"
+				}
+
+				log.Printf("%s [%s]\n", b.URL, status)
+			}
+
+			log.Printf("health check if done...\n")
+		}
+	}
 }
